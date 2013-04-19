e0dcedf486169ae8842d9e59120dc2b25f0967cd
Infinity <root@infinitylabs.us>
2013-04-19 10:32:50 -0600

    Yandere update

--- plugins/yandere.py
+++ plugins/yandere.py
@@ -14,7 +14,7 @@ def refresh_cache():
         title = result.find('img', {'class': re.compile(r'\bpreview\b')}) #['title']
         img = result.find('a', {'class': re.compile(r'\bdirectlink\b')}) #['href']
         if img and title:
-            yandere_cache.append((result['id'].replace('p','') ,title['title'], img['href']))
+            yandere_cache.append((result['id'].replace('p','') ,title['title'].split(' User')[0], img['href']))
 
 # do an initial refresh of the cache
 refresh_cache()
@@ -24,6 +24,6 @@ def yandere(inp, reply=None):
     "Yande.re -- Gets a random image from Yande.re."
 
     id, title, image = yandere_cache.pop()
-    reply('\x02(%s)\x02 %s: %s' % (id, title, web.isgd(image)))
+    reply('\x034NSFW\x03: \x02(%s)\x02 %s: %s' % (id, title, web.isgd(image)))
     if len(yandere_cache) < 3:
         refresh_cache()
