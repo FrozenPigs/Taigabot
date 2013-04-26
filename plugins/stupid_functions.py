03369e95f9ddae10f05fe9dcf73a6ee6632ce0e0
Infinity <root@infinitylabs.us>
2013-04-26 00:38:42 -0600

    Stupid functions update

--- plugins/stupid_functions.py
+++ plugins/stupid_functions.py
@@ -1,18 +1,28 @@
 from util import hook
 
 @hook.regex(r'^(same)$')
-def same(inp):
+def same(inp,bot=None,chan=None):
     "<word>? -- Shows what data is associated with <word>."
+    try: disabled_channel_commands = bot.channelconfig[chan.lower()]['disabled_commands']
+    except: disabled_channel_commands = " "
+    if 'same' in disabled_channel_commands: return None
     return 'same'
+    
 
 @hook.regex(r'^(HUEHUEHUE)$')
 @hook.regex(r'^(huehuehue)$')
-def hue(inp):
+def hue(inp,bot=None,chan=None):
     "<word>? -- Shows what data is associated with <word>."
+    try: disabled_channel_commands = bot.channelconfig[chan.lower()]['disabled_commands']
+    except: disabled_channel_commands = " "
+    if 'hue' in disabled_channel_commands: return None
     return inp.group(0)
 
 @hook.regex(r'^(TETETE)$')
 @hook.regex(r'^(tetete)$')
-def tetete(inp, nick=None):
+def tetete(inp, nick=None,bot=None,chan=None):
     "<word>? -- Shows what data is associated with <word>."
+    try: disabled_channel_commands = bot.channelconfig[chan.lower()]['disabled_commands']
+    except: disabled_channel_commands = " "
+    if 'tetete' in disabled_channel_commands: return None
     return 'tetete %s%s%s' % (nick, nick, nick)
