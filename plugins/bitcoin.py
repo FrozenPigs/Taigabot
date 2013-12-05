0a886ba1bfd8d50b97072b87dc22a9cb4b51b01e
Infinity <root@infinitylabs.us>
2013-12-05 00:28:20 -0700

    Updated litecoin

--- plugins/bitcoin.py
+++ plugins/bitcoin.py
@@ -33,25 +33,18 @@ def btc(inp, say=None):
 @hook.command(autohelp=False)
 def ltc(inp, say=None):
     "litecoin -- gets current exchange rate for litecoins from mtgox"
-    # data = http.get_json("http://data.mtgox.com/api/2/LTCUSD/money/ticker")
-    data = http.get_json("https://btc-e.com/api/2/ltc_usd/ticker"
-    
+    data = http.get_json("https://btc-e.com/api/2/ltc_usd/ticker")
     data = data['ticker']
+    print data
     ticker = {
-        'buy': data['buy']['display_short'],
-        'high': data['high']['display_short'],
-        'low': data['low']['display_short'],
-        'vol': data['vol']['display_short'],
+        'buy': data['buy'],
+        'high': data['high'],
+        'low': data['low'],
+        'vol': data['vol'],
     }
-
-    #data = http.get_json("http://data.mtgox.com/api/1/generic/order/lag")
-    #data = data['return']
-    #lag = {
-    #    'lag': data['lag_text']
-    #}
+    print ticker
 
     result = ("Current: \x0307%(buy)s\x0f - High: \x0307%(high)s\x0f"
         " - Low: \x0307%(low)s\x0f - Volume: %(vol)s" % ticker)
-    #result_lag = ("Lag: %(lag)s" % lag)
 
     say("%s" % (result))
