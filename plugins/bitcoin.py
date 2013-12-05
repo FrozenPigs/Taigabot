59877ae8d5961d4d54756f7f76c6f86f9a7aebc9
Infinity <root@infinitylabs.us>
2013-12-05 00:12:14 -0700

    Added Litecoin

--- plugins/bitcoin.py
+++ plugins/bitcoin.py
@@ -5,9 +5,35 @@ from util import http, hook
 @hook.command('bitcoin',autohelp=False)
 @hook.command('buttcoin',autohelp=False)
 @hook.command(autohelp=False)
-def bc(inp, say=None):
+def btc(inp, say=None):
     "bitcoin -- gets current exchange rate for bitcoins from mtgox"
     data = http.get_json("http://data.mtgox.com/api/2/BTCUSD/money/ticker")
+    data = data['data']
+    ticker = {
+        'buy': data['buy']['display_short'],
+        'high': data['high']['display_short'],
+        'low': data['low']['display_short'],
+        'vol': data['vol']['display_short'],
+    }
+
+    data = http.get_json("http://data.mtgox.com/api/1/generic/order/lag")
+    data = data['return']
+    lag = {
+        'lag': data['lag_text']
+    }
+
+    result = ("Current: \x0307%(buy)s\x0f - High: \x0307%(high)s\x0f"
+        " - Low: \x0307%(low)s\x0f - Volume: %(vol)s" % ticker)
+    result_lag = ("Lag: %(lag)s" % lag)
+
+    say("%s - %s" % (result, result_lag))
+
+
+@hook.command('litecoin',autohelp=False)
+@hook.command(autohelp=False)
+def ltc(inp, say=None):
+    "litecoin -- gets current exchange rate for litecoins from mtgox"
+    data = http.get_json("http://data.mtgox.com/api/2/LTCUSD/money/ticker")
     data = data['data']
     ticker = {
         'buy': data['buy']['display_short'],
