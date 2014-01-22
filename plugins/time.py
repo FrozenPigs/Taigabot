9a341a71a36060fd20af32e646d2351f5b2d3aab
Infinity <root@infinitylabs.us>
2014-01-21 20:07:35 -0700

    Added Kernel

--- plugins/time.py
+++ plugins/time.py
@@ -40,7 +40,6 @@ def time(inp, nick="", reply=None, db=None, notice=None):
         url = "https://www.google.com/search?q=time+in+%s" % location.replace(' ','+')
         html = http.get_html(url)
         prefix = html.xpath("//div[@class='vk_c vk_gy vk_sh _v']//span[@class='vk_gy vk_sh']/text()")[0].strip()
-        print prefix
         time = html.xpath("//div[@class='vk_c vk_gy vk_sh _v']//div[@class='vk_bk vk_ans']/text()")[0].strip()
         day = html.xpath("//div[@class='vk_c vk_gy vk_sh _v']//div[@class='vk_gy vk_sh']/text()")[0].strip()
         date = html.xpath("//div[@class='vk_c vk_gy vk_sh _v']//div[@class='vk_gy vk_sh']/span/text()")[0].strip()
