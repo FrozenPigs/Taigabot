3e38f912f1bed1ade65c71a5bda4cd71d9eb57fa
Scott Steiner <nothingfinerthanscottsteiner@gmail.com>
2015-02-10 22:50:56 -0500

    Remove crap

--- plugins/util/database.py
+++ plugins/util/database.py
@@ -19,15 +19,9 @@ def init(db):
         db.commit()
         db_ready = True
 
-def init_tables(db, table):
-    if "channels" in table: db.execute("INSERT INTO channels VALUES(?,'','','','','','','','','','')", [chan])
-    # nick, mask, version, location, lastfm, fines, battlestation, desktop, horoscope, primary key(nick)
-    db.commit()
-
 def field_exists(db,table,matchfield,matchvalue):
     init(db)
     exists = db.execute("SELECT EXISTS(SELECT 1 FROM {} WHERE {}='{}' LIMIT 1);".format(table,matchfield,matchvalue.encode('utf8'))).fetchone()[0]
-    # print "Exists: '{}'".format(exists)
     if exists: return True
     else: return False
 
@@ -35,7 +29,6 @@ def get(db,table,field,matchfield,matchvalue):
     init(db)
     matchvalue = matchvalue.encode('utf-8').lower()
     try:
-        #print "SELECT {} FROM {} WHERE {}='{}';".format(field,table,matchfield,matchvalue.lower())
         result = db.execute("SELECT {} FROM {} WHERE {}='{}';".format(field,table,matchfield,matchvalue)).fetchone()
         if result: return result[0]
         else: return False
@@ -50,14 +43,10 @@ def set(db, table, field, value, matchfield, matchvalue):
     if type(value) is str: value = value.replace("'","").replace('\"', "").encode('utf-8')
     try:
         if field_exists(db,table,matchfield,matchvalue):
-            # print "UPDATE {} SET {} = '{}' WHERE {} = '{}';".format(table,field,value,matchfield,matchvalue.encode('utf8').lower())
             db.execute("UPDATE {} SET {} = '{}' WHERE {} = '{}';".format(table,field,value,matchfield,matchvalue))
         else: 
-            # print "INSERT INTO {} ({},{}) VALUES ('{}','{}');".format(table,field,matchfield,value,matchvalue.encode('utf8').lower())
             db.execute("INSERT INTO {} ({},{}) VALUES ('{}','{}');".format(table,field,matchfield,value,matchvalue))
     except:
-        #print "DB"
-        # print "UPDATE {} SET {} = '{}' WHERE {} = '{}';".format(table,field,value,matchfield,matchvalue.encode('utf8').lower())
         db.execute("UPDATE {} SET {} = '{}' WHERE {} = '{}';".format(table,field,value,matchfield,matchvalue))
         
     db.commit()
