eabd535012bf7907bf9878d3d92813f372516992
infinity <root@infinitylabs.us>
2015-05-04 01:40:45 +0000

    Fixed formatting.py

--- plugins/util/formatting.py
+++ plugins/util/formatting.py
@@ -1,9 +1,9 @@
 def filesize(num, suffix='B'):
-	for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
-		if abs(num) < 1024.0:
-			return '{0:.2f}{1}{2}'.format(num, unit, suffix)
-		num /= 1024.0
-	return '{0:.1f}{}{}'.format(num, 'Yi', suffix)
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return '{0:.2f}{1}{2}'.format(num, unit, suffix)
+        num /= 1024.0
+    return '{0:.1f}{}{}'.format(num, 'Yi', suffix)
 
 def output(scriptname, fields=[], color=11):
-	return '[{}] {}'.format(scriptname, ' - '.join(fields))
+    return '[{}] {}'.format(scriptname, ' - '.join(fields))
