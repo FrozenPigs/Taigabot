3257ed05d4aa70a1f0b19435751bd40ac6c76c66
ararouge <64179999+ararouge@users.noreply.github.com>
2020-05-02 19:02:03 -0500

    Fixed bug where cache would always be refreshed

--- plugins/furry.py
+++ plugins/furry.py
@@ -10,8 +10,8 @@ def refresh_cache(inp):
     global lastsearch
     furry_cache = []
     num = 0
+    lastsearch = inp
     search = inp.replace(' ','%20').replace('explicit','rating:explicit').replace('nsfw','rating:explicit').replace('safe','rating:safe').replace('sfw','rating:safe').replace('questionable','rating:questionable')
-    lastsearch = search
     if inp == '':
         postjson = http.get_json('http://e621.net/posts.json?limit=20')
     else:
