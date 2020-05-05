e2971050e9355b2de8ea95e9ff3f668f49a2218b
inexist3nce <inexist3nce@users.noreply.github.com>
2020-05-05 03:00:49 -0400

    new utility for http, uses requests.get() instead of open()

---
+++ plugins/util/request.py
@@ -0,0 +1,28 @@
+from urllib import quote
+import requests
+
+fake_ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3745.0 Safari/537.36'
+
+
+def urlencode(inp):
+    def force_decode(string, codecs=['utf8', 'cp1252']):
+        for i in codecs:
+            try:
+                return string.decode(i)
+            except UnicodeDecodeError:
+                pass
+
+    if isinstance(inp, str):
+        inp = force_decode(inp)
+
+    return quote(inp.encode('utf8'))
+
+
+def get_html(url):
+    return get_text(url)
+
+
+def get_text(url):
+    headers = {'User-Agent': fake_ua}
+    r = requests.get(url, headers=headers)
+    return r.text
