74838832b2731374cec09f5411d00c9718bb1037
inexist3nce <inexist3nce@users.noreply.github.com>
2020-05-05 03:01:22 -0400

    fix distance, since google paywalled location apis

--- plugins/distance.py
+++ plugins/distance.py
@@ -1,39 +1,41 @@
-from util import hook, http
-import re, math
+from util import hook, http, request
+from bs4 import BeautifulSoup
 
-def get_distance(origin, destination):
-    lat1, lon1 = origin
-    lat2, lon2 = destination
-    radius = 6371 # km
 
-    dlat = math.radians(lat2-lat1)
-    dlon = math.radians(lon2-lon1)
-    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
-        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    d = radius * c
+def fetch(start, dest):
+    start = request.urlencode(start)
+    dest = request.urlencode(dest)
+    url = "http://www.travelmath.com/flying-distance/from/%s/to/%s" % (start, dest)
+    html = request.get_html(url)
+    return html
 
-    return d
 
-def get_location(location):
-    location_data = http.get_json("http://maps.googleapis.com/maps/api/geocode/json?address={}&sensor=false".format(location))['results'][0]
-    location_name = location_data['formatted_address']
-    Location_latlong = [location_data['geometry']['location']['lat'], location_data['geometry']['location']['lng']]
-    return (location_name, Location_latlong)
+def parse(html):
+    soup = BeautifulSoup(html, 'lxml')
+    query = soup.find('h1', {'class': 'main'})
+    distance = soup.find('h3', {'class': 'space'})
+
+    if query:
+        query = query.get_text()
+
+    if distance:
+        distance = distance.get_text()
+
+    return query, distance
+
 
 @hook.command
 def distance(inp):
-    """distance from <origin> to <dest> -- calculates the distance between any locations"""
+    "distance <start> to <end> -- Calculate the distance between 2 places."
     if 'from ' in inp: inp = inp.replace('from ','')
-    inp = inp.replace(', ','+')
-    orig = inp.split(" to ")[0].strip().replace(' ','+')
-    dest = inp.split(" to ")[1].strip().replace(' ','+')
-    try:
-        orig_location, orig_latlong = get_location(orig)
-        dest_location, dest_latlong = get_location(dest)
-        distance_km = get_distance(orig_latlong, dest_latlong)
-        distance_mile = distance_km * 0.621371
+    start = inp.split(" to ")[0].strip()
+    dest = inp.split(" to ")[1].strip()
 
-        return u"The distance from \x02%s\x02 to \x02%s\x02 is: %0.2f miles / %0.2f km" % (orig_location, dest_location, distance_mile, distance_km)
-    except:
-        return "Cannot get distance from {} to {}".format(orig,dest)
+    html = fetch(start, dest)
+    query, distance = parse(html)
+
+    if not distance:
+        return "Could not calculate the distance from %s to %s." % (start, dest)
+
+    result = "Distance: %s %s" % (query, distance)
+    return result
