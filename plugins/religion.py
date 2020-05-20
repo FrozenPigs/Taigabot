1d9b7ea36d231c72a726443db7d5ae6743d30f86
inexist3nce <inexist3nce@users.noreply.github.com>
2020-05-19 20:46:08 -0400

    rewrite koran, drop urllib, use https
    the english bible api is broken because we need to get our "app accepted" or some shit
    https://api.esv.org/docs/

--- plugins/religion.py
+++ plugins/religion.py
@@ -1,33 +1,56 @@
-from util import hook, http
-import requests
-import urllib
+from util import hook, request
+from bs4 import BeautifulSoup
 
+# TODO get a working api key
+# u need like a church email to register or something idk
+# also move it to the config file
+API_KEY = "e1733632cb3a360e8d4db70b13da07f2df5c785d"
 
 @hook.command('god')
 @hook.command
 def bible(inp):
     """.bible <passage> -- gets <passage> from the Bible (ESV)"""
 
-    text = requests.get("https://api.esv.org/v3/passage/html/?q={}".format(urllib.quote(inp)), headers={"Authorization": "Token e1733632cb3a360e8d4db70b13da07f2df5c785d"})
-    print(text.text)
-    text = ' '.join(text.split())
+    url = "https://api.esv.org/v3/passage/text/?q=" + request.urlencode(inp)
+    json = request.get_json(url, headers={"Authorization": "Token " + API_KEY})
 
-    if len(text) > 400:
-        text = text[:text.rfind(' ', 0, 400)] + '...'
+    if 'detail' in json:
+        return 'Bible error (lol): ' + json['detail']
 
-    return text
+    output = '[Bible]'
+
+    if 'canonical' in json:
+        output = output + '\x02' + json['canonical'] + '\x02:'
+
+    if 'passages' in json:
+        output = output + '. '.join(json['passages'])
+
+    if len(output) > 320:
+        output = output[:320] + '...'
+
+    return output
 
 
 @hook.command('allah')
 @hook.command
 def koran(inp):  # Koran look-up plugin by Ghetto Wizard
-    ".koran <chapter.verse> -- gets <chapter.verse> from the Koran"
+    ".koran <chapter.verse> -- gets <chapter.verse> from the Koran. it can also search any text."
 
-    url = 'http://quod.lib.umich.edu/cgi/k/koran/koran-idx?type=simple'
+    url = 'https://quod.lib.umich.edu/cgi/k/koran/koran-idx?type=simple&q1='
 
-    results = http.get_html(url, q1=inp).xpath('//li')
+    html = request.get_html(url + request.urlencode(inp))
+    soup = BeautifulSoup(html, 'lxml')
+    query = soup.find_all('li')
 
-    if not results:
+    if not query or len(query) == 0:
         return 'No results for ' + inp
 
-    return results[0].text_content()
+    output = '[Koran] '
+
+    for li in query:
+        output = output + ' ' + li.text
+
+    if len(output) > 320:
+        output = output[:320] + '...'
+
+    return output
