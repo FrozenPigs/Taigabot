ad30922b8a3d53ec0238abff86175a4efdca4a0b
inexist3nce <inexist3nce@users.noreply.github.com>
2020-05-31 03:42:01 -0400

    formatting, fix broken code

--- plugins/countdown.py
+++ plugins/countdown.py
@@ -3,6 +3,7 @@ import time
 
 countdown_is_running = False
 countdown_nicks = []
+countdown_timeout = 20
 
 
 def set_countdown_to_true():
@@ -17,8 +18,7 @@ def set_countdown_to_false():
 
 @hook.command(autohelp=False)
 def countdown(inp, me=None):
-    "countdown [seconds] [nick1 nick2 nick3]-- does a countdown for the channel" \
-    "The countdown will begin when all the users type .ready"
+    "countdown [seconds] [nick1 nick2 nick3] -- starts a countdown. It will begin when all the users type .ready"
 
     if countdown_is_running:
         return
@@ -39,24 +39,27 @@ def countdown(inp, me=None):
         else:
             countdown_nicks = inp.split()[0:]
             count = 6
-    except:
+    except Exception:
         pass
 
     if len(inp) > 6:
-        me('Countdown started! Waiting for %s. Type \x02.ready\x02 when ready!' % ', '.join(countdown_nicks))
+        nicks = ', '.join(countdown_nicks)
+        me('Countdown started! Waiting for {}. Type \x02.ready\x02 when ready!'.format(nicks))
+
+        # TODO fix: there can be only one countdown running per network
+        # this chunk keeps the whole plugin busy
         while countdown_nicks:
             time.sleep(1)
             wait_count = int(wait_count) + 1
-            if wait_count == 30:
+            if wait_count == countdown_timeout:
                 set_countdown_to_false()
                 return "Countdown expired."
-                break
 
         me('Ready! The countdown will begin in 2 seconds...')
         time.sleep(2)
 
     for cur in range(1, count):
-        me('*** %s ***' % (count - cur))
+        me('*** {} ***'.format(count - cur))
         time.sleep(1)
     else:
         set_countdown_to_false()
@@ -70,9 +73,9 @@ def ready(inp, me=None, nick=None):
     nicks_size_start = len(countdown_nicks)
     try:
         countdown_nicks.remove(nick.lower())
-    except:
+    except Exception:
         pass
 
     if nicks_size_start > len(countdown_nicks):
         if len(countdown_nicks) > 0:
-            me('Waiting for: %s' % ', '.join(countdown_nicks))
+            me('Waiting for: ' + ', '.join(countdown_nicks))
