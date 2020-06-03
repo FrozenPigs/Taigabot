50d3a16a6afd34644f300a1c63b1612e7eda4216
inexist3nce <inexist3nce@users.noreply.github.com>
2020-06-03 12:32:15 -0400

    google: clean a bit, use requests

--- plugins/google.py
+++ plugins/google.py
@@ -1,40 +1,32 @@
-from util import hook, http, database, web
+from util import hook, web
+from utilities import request, formatting
+import re
 
-imp_reg = (r'^\>(.*\.(gif|GIF|jpg|JPG|jpeg|JPEG|png|PNG|tiff|TIFF|bmp|BMP))'
-           r'\s?(\d+)?')
+imp_reg = (r'^\>(.*\.(gif|jpe?g|png|tiff|bmp))$', re.I)
+API_URL = 'https://www.googleapis.com/customsearch/v1'
 
 
 def api_get(kind, query):
     """Use the RESTful Google Search API."""
     if kind == 'image':
-        url = ('https://www.googleapis.com/customsearch/v1?key={}&cx={}'
-               '&searchType={}&num=1&safe=off&q={}')
-        return http.get_json(url.format(query[0], query[1], kind, query[2]))
+        url = API_URL + u'?key={}&cx={}&searchType={}&num=1&safe=off&q={}'
+        return request.get_json(url.format(query[0], query[1], kind, query[2]))
     elif kind == 'images':
-        url = ('https://www.googleapis.com/customsearch/v1?key={}&cx={}'
-               '&searchType={}&num=1&safe=off&q={}&fileType="{}"')
-        return http.get_json(url.format(query[0], query[1], 'image', query[2], query[3]))
+        url = API_URL + u'?key={}&cx={}&searchType={}&num=1&safe=off&q={}&fileType="{}"'
+        return request.get_json(url.format(query[0], query[1], 'image', query[2], query[3]))
     else:
-        url = ('https://www.googleapis.com/customsearch/v1?key={}&cx={}'
-               '&num=1&safe=off&q={}')
-        return http.get_json(url.format(query[0], query[1], query[2].encode('utf-8')))
-        # url = ('http://ajax.googleapis.com/ajax/services/search/{}?'
-        #        'v=1.0&safe=off&q={}')
-        # return http.get_json(url.format(kind, query))
-
+        url = API_URL + u'?key={}&cx={}&num=1&safe=off&q={}'
+        return request.get_json(url.format(query[0], query[1], query[2].encode('utf-8')))
 
 
 @hook.command('search')
 @hook.command('g')
-@hook.command('wiki')
 @hook.command
-def google(inp, bot=None, db=None, chan=None, paraml=None):
+def google(inp, bot=None):
     """google <query> -- Returns first google search result for <query>."""
-    command = paraml[-1].split(' ')[0][1:].lower()
-    if command == 'wiki':
-        inp = 'site:wikipedia.org {}'.format(inp)
-    trimlength = database.get(db,'channels','trimlength','chan',chan)
-    if not trimlength: trimlength = 9999
+    inp = request.urlencode(inp)
+
+    # what the fuck
     try:
         cx = bot.config['api_keys']['googleimage']
         key = bot.config['api_keys']['google']
@@ -45,68 +37,49 @@ def google(inp, bot=None, db=None, chan=None, paraml=None):
         key = bot.config['api_keys']['google2']
         query = [key, cx, '+'.join(inp.split())]
         result = api_get('None', query)['items'][0]
+
     title = result['title']
-    content = http.html.fromstring(result['snippet'].replace('\n', '')).text_content()
+    content = formatting.remove_newlines(result['snippet'])
     link = result['link']
+
     try:
         return u'{} -- \x02{}\x02: "{}"'.format(web.isgd(link), title, content)
-    except:
+    except Exception:
         return u'{} -- \x02{}\x02: "{}"'.format(link, title, content)
 
-    # parsed = api_get('web', '+'.join(inp.split()))
-    # if not 200 <= parsed['responseStatus'] < 300:
-    #     raise IOError('error searching for pages: {}: {}'.format(parsed['responseStatus'], ''))
-    # if not parsed['responseData']['results']:
-    #     return 'No results found.'
 
-    # result = parsed['responseData']['results'][0]
-    # title = http.unescape(result['titleNoFormatting'])
-    # content = http.unescape(result['content'])
-
-    # if not content: content = "No description available."
-    # else: content = http.html.fromstring(content.replace('\n', '')).text_content()
-
-    # try:
-    #     return u'{} -- \x02{}\x02: "{}"'.format(web.isgd(result['unescapedUrl']), title, content)
-    # except:
-    #     return u'{} -- \x02{}\x02: "{}"'.format(result['unescapedUrl'], title, content)
-
+@hook.command('wiki')
+def fake_wikipedia(inp, bot=None):
+    """wiki <query> -- search wikipedia"""
+    inp = u'site:wikipedia.org {}'.format(inp)
+    return google(inp, bot)
 
 
 @hook.command('gi')
-def image(inp, reply=None, bot=None):
+def image(inp, bot=None):
     """image <query> -- Returns the first Google Image result for <query>."""
     try:
         cx = bot.config['api_keys']['googleimage']
         key = bot.config['api_keys']['google']
         query = [key, cx, '+'.join(inp.split())]
         image = api_get('image', query)['items'][0]['link']
-    except:
+    except Exception:
         cx = bot.config['api_keys']['googleimage']
         key = bot.config['api_keys']['google2']
         query = [key, cx, '+'.join(inp.split())]
         image = api_get('image', query)['items'][0]['link']
-        #url = ('https://www.google.co.uk/search?tbm=isch&hl=en-GB&source=hp&bi'
-        #       'w=&bih=&q={0}&gbv=2&oq={0}&gs_l=img.3..0l10.1471.1673.0.1792.3'
-        #       '.3.0.0.0.0.86.231.3.3.0....0...1ac.1.34.img..0.3.231.tFWV7YPBE'
-        #       'c8')
-        #url = url.format('+'.join(inp.split()))
-        #soup = http.get_soup(url)
-        #for link in soup.find_all('a'):
-        #    try:
-        #        if link.get('class')[0] == 'rg_l':
-        #            image = link.get('href')[15:].split('&')[0]
-        #            break
-        #    except TypeError:
-        #        pass
+
+    print image
+
     try:
-        reply(web.isgd(image))
-    except:
-        reply(image)
+        return web.isgd(image)
+    except Exception as e:
+        print '[!] Error while shortening:', e
+        return image
 
 
-@hook.regex(imp_reg)
-def implying(inp, reply=None, bot=None):
+@hook.regex(*imp_reg)
+def implying(inp, bot=None):
     """><query>.jpg -- Returns the first Google Image result for <query>.jpg"""
     inp = inp.string[1:].split('.')
     filetype = inp[1]
@@ -114,7 +87,9 @@ def implying(inp, reply=None, bot=None):
     key = bot.config['api_keys']['google']
     query = [key, cx, '+'.join(inp[0].split()), filetype]
     image = api_get('images', query)['items'][0]['link']
+
     try:
-        reply(web.isgd(image))
-    except:
-        reply(image)
+        return web.isgd(image)
+    except Exception as e:
+        print '[!] Error while shortening:', e
+        return image
