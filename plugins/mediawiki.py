d9caa471598464680753d2a0e3bcd54484790699
inexist3nce <inexist3nce@users.noreply.github.com>
2020-06-05 08:01:55 -0400

    change formatting as requested by weds

--- plugins/mediawiki.py
+++ plugins/mediawiki.py
@@ -73,9 +73,9 @@ def command_wrapper(instance, inp):
         output = output[:OUTPUT_LIMIT] + '...'
 
     if title == real_title:
-        return u'\x02{}\x02 :: {}'.format(title, output)
+        return u'\x02{} -\x02 {} \x02-\x02 {}'.format(title, output, url)
     else:
-        return u'\x02{}\x02 :: {} (redirected from {})'.format(real_title, output, title)
+        return u'\x02{} -\x02 {} \x02-\x02 {} (redirected from {})'.format(real_title, output, url, title)
 
 
 def url_wrapper(instance, url):
