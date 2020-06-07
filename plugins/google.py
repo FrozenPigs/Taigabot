7af0d4dac5c0a7a62ea10e00a5dec6600220b814
wednesday <frozenpigs@cock.li>
2020-06-07 21:41:54 +0000

    Clean up google.py, add potato.py

--- plugins/google.py
+++ plugins/google.py
@@ -1,22 +1,9 @@
-from util import hook, web
-from utilities import request, formatting
 import re
 
-imp_reg = (r'^\>(.*\.(gif|jpe?g|png|tiff|bmp))$', re.I)
+from util import hook, web
+from utilities import formatting, request
+
 API_URL = 'https://www.googleapis.com/customsearch/v1'
-
-
-def api_get(kind, query):
-    """Use the RESTful Google Search API."""
-    if kind == 'image':
-        url = API_URL + u'?key={}&cx={}&searchType={}&num=1&safe=off&q={}'
-        return request.get_json(url.format(query[0], query[1], kind, query[2]))
-    elif kind == 'images':
-        url = API_URL + u'?key={}&cx={}&searchType={}&num=1&safe=off&q={}&fileType="{}"'
-        return request.get_json(url.format(query[0], query[1], 'image', query[2], query[3]))
-    else:
-        url = API_URL + u'?key={}&cx={}&num=1&safe=off&q={}'
-        return request.get_json(url.format(query[0], query[1], query[2].encode('utf-8')))
 
 
 @hook.command('search')
@@ -26,17 +13,11 @@ def google(inp, bot=None):
     """google <query> -- Returns first google search result for <query>."""
     inp = request.urlencode(inp)
 
-    # what the fuck
-    try:
-        cx = bot.config['api_keys']['googleimage']
-        key = bot.config['api_keys']['google']
-        query = [key, cx, '+'.join(inp.split())]
-        result = api_get('None', query)['items'][0]
-    except:
-        cx = bot.config['api_keys']['googleimage']
-        key = bot.config['api_keys']['google2']
-        query = [key, cx, '+'.join(inp.split())]
-        result = api_get('None', query)['items'][0]
+    url = API_URL + u'?key={}&cx={}&num=1&safe=off&q={}'
+    cx = bot.config['api_keys']['googleimage']
+    search = '+'.join(inp.split())
+    key = bot.config['api_keys']['google']
+    result = request.get_json(url.format(key, cx, search.encode('utf-8')))['items'][0]
 
     title = result['title']
     content = formatting.remove_newlines(result['snippet'])
@@ -48,41 +29,28 @@ def google(inp, bot=None):
         return u'{} -- \x02{}\x02: "{}"'.format(link, title, content)
 
 
+@hook.regex(r'^\>(.*\.(gif|jpe?g|png|tiff|bmp))$', re.I)
 @hook.command('gi')
 def image(inp, bot=None):
     """image <query> -- Returns the first Google Image result for <query>."""
-    try:
+    if type(inp) is unicode:
+        filetype = None
+    else:
+        inp, filetype = inp.string[1:].split('.')
+
         cx = bot.config['api_keys']['googleimage']
-        key = bot.config['api_keys']['google']
-        query = [key, cx, '+'.join(inp.split())]
-        image = api_get('image', query)['items'][0]['link']
-    except Exception:
-        cx = bot.config['api_keys']['googleimage']
-        key = bot.config['api_keys']['google2']
-        query = [key, cx, '+'.join(inp.split())]
-        image = api_get('image', query)['items'][0]['link']
-
-    print image
-
-    try:
-        return web.isgd(image)
-    except Exception as e:
-        print '[!] Error while shortening:', e
-        return image
-
-
-@hook.regex(*imp_reg)
-def implying(inp, bot=None):
-    """><query>.jpg -- Returns the first Google Image result for <query>.jpg"""
-    inp = inp.string[1:].split('.')
-    filetype = inp[1]
-    cx = bot.config['api_keys']['googleimage']
+    search = '+'.join(inp.split())
     key = bot.config['api_keys']['google']
-    query = [key, cx, '+'.join(inp[0].split()), filetype]
-    image = api_get('images', query)['items'][0]['link']
+
+    if filetype:
+        url = API_URL + u'?key={}&cx={}&searchType=image&num=1&safe=off&q={}&fileType={}'
+        result = request.get_json(url.format(key, cx, search.encode('utf-8'),
+                                             filetype))['items'][0]['link']
+    else:
+        result = request.get_json(url.format(key, cx, search.encode('utf-8')))['items'][0]['link']
 
     try:
-        return web.isgd(image)
+        return web.isgd(result)
     except Exception as e:
         print '[!] Error while shortening:', e
-        return image
+        return result
